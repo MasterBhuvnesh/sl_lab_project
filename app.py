b2b18b69518facbd719e673d1f7753f1d7dc8d93
Bhuvnesh verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2025-02-11 15:45:11 +0530

    modify the code

--- app.py
+++ app.py
@@ -3,11 +3,54 @@ import json
 
 app = Flask(__name__)
 
-def load_timetable():
-    with open("timetable.json", "r") as file:
-        return json.load(file)
+TIMETABLE={
+  "Monday": [
+    { "time": "8:00-9:00", "subject": "Minor", "room": "DT-304" },
+    { "time": "9:00-10:00", "subject": "HUT4003 - ME", "room": "DT-206" },
+    { "time": "10:00-11:00", "subject": "CAT4002 - DAA", "room": "DT-206" },
+    { "time": "11:00-12:00", "subject": "RECESS" },
+    { "time": "12:00-1:00", "subject": "CAT4003 - TOC", "room": "DT-206" },
+    { "time": "1:00-2:00", "subject": "CAT4001 - AI", "room": "DT-206" },
+    { "time": "2:00-3:00", "subject": "MAT4001 - LA", "room": "DT-206" }
+  ],
+  "Tuesday": [
+    { "time": "8:00-9:00", "subject": "OE", "room": "DT-304" },
+    { "time": "9:00-10:00", "subject": "CAT4003 - TOC", "room": "DT-206" },
+    { "time": "10:00-11:00", "subject": "CAT4002 - DAA", "room": "DT-206" },
+    { "time": "11:00-12:00", "subject": "HUT4003 - ME", "room": "DT-206" }
+  ],
+  "Wednesday": [
+    { "time": "8:00-9:00", "subject": "HUT4002 - EE", "room": "DT-206" },
+    { "time": "9:00-10:00", "subject": "CAT4002 - DAA", "room": "DT-206" },
+    { "time": "10:00-11:00", "subject": "RECESS" },
+    { "time": "11:00-12:00", "subject": "CAT4001 - AI", "room": "DT-206" },
+    { "time": "12:00-1:00", "subject": "MAT4001 - LA", "room": "DT-206" }
+  ],
+  "Thursday": [
+    { "time": "8:00-9:00", "subject": "Minor", "room": "DT-304" },
+    { "time": "9:00-10:00", "subject": "HUT4003 - ME", "room": "DT-206" },
+    { "time": "10:00-11:00", "subject": "CAT4002 - DAA", "room": "DT-206" },
+    { "time": "11:00-12:00", "subject": "RECESS" },
+    { "time": "12:00-1:00", "subject": "CAT4003 - TOC", "room": "DT-206" },
+    { "time": "1:00-2:00", "subject": "CAT4001 - AI", "room": "DT-206" }
+  ],
+  "Friday": [
+    { "time": "8:00-9:00", "subject": "MAT4001 - LA", "room": "DT-206" },
+    { "time": "9:00-10:00", "subject": "CAT4003 - TOC", "room": "DT-206" },
+    { "time": "10:00-11:00", "subject": "RECESS" },
+    { "time": "11:00-12:00", "subject": "HUT4002 - EE", "room": "DT-206" },
+    { "time": "12:00-1:00", "subject": "CAT4002 - DAA", "room": "DT-206" }
+  ],
+  "Saturday": [
+    { "time": "8:00-9:00", "subject": "HUT4003 - ME", "room": "DT-206" },
+    { "time": "9:00-10:00", "subject": "CAT4002 - DAA", "room": "DT-206" },
+    { "time": "10:00-11:00", "subject": "RECESS" },
+    { "time": "11:00-12:00", "subject": "CAT4001 - AI", "room": "DT-206" },
+    { "time": "12:00-1:00", "subject": "MAT4001 - LA", "room": "DT-206" }
+  ]
+}
 
-TIMETABLE = load_timetable()
+
 
 @app.route('/')
 def home():
